d12cd24789708784f1d92aa84573aa71120b8b18
ArySuarsyah <89743360+ArySuarsyah@users.noreply.github.com>
2022-11-07 12:48:27 +0800

    Add files via upload

---
+++ Asset/css/builtInFunction.js
@@ -0,0 +1,7 @@
+// .length = berfungsi untuk mengaetahui atau menghitung panjang sebuah array, maupun string
+//  .indexOf = digunakan untuk mencari tau posisi index string maupun array dengan mengetikkan karakter atau string yang ingin dicari posisinya, jika ada dua karakter atau kata yang sama maka yang pertama kali ditemukan yang akan diambil indexnya.
+
+
+
+// toString = Merubah string array menjadi string
+// pop = berguna untuk menghapus index/element terakhir pada sebuah array
---
+++ Asset/script/script.js
@@ -0,0 +1,7 @@
+const profil = document.querySelector('.profil')
+const dropMenu = document.querySelector('.btn-nav div');
+
+
+profil.addEventListener('click', function() {
+    dropMenu.innerHTML = <div>alalal</div>
+})
