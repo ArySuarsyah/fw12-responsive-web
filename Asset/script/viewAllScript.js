293ab931aae1512ae744b3dd272ec61ebb6305c8
arsrsyh <gotrany97@gmail.com>
2022-11-17 20:16:18 +0800

    import data API

--- Asset/script/viewAllScript.js
+++ Asset/script/viewAllScript.js
@@ -8,37 +8,50 @@ const getMovieData = fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=
 const proses = async () => {
     const respons = await getMovieData;
     const data = await respons.json();
-    list.innerHTML = data.original_title
+    list.innerHTML = data.poster
     
+    let movie = data.results
+    console.log(movie);
+    movie.forEach((el) => {
+        const figureViewAll = document.createElement(`figure`);
+        list.appendChild(figureViewAll);
+        
+        const picture = document.createElement('img');
+        figureViewAll.appendChild(picture);
+        // picture.src = el.poster;
+        
+        
+        const divInfo = document.createElement('div');
+        figureViewAll.appendChild(divInfo);
+        
+        
+        const title = document.createElement('figcaption');
+        title.className = 'movieTitle';
+        title.innerText = movie.title
+        
+        divInfo.appendChild(title);
+        const genre = document.createElement('figcaption');
+        genre.className = 'movieGenre';
+        genre.innerText = el.genre;
+        divInfo.appendChild(genre);
+        
+        
+        const linkDetail = document.createElement('a');
+        divInfo.appendChild(linkDetail);
+        linkDetail.href = el.link;
+
+
+        const btnCard = document.createElement('button');
+        linkDetail.appendChild(btnCard);
+        btnCard.className = 'btn-detail';
+        btnCard.innerText = 'Details';
+        btnCard.type = 'submit';
+        
+    });
 };
-
-
-
-data.forEach(el => {
-
-    let figureViewAll = document.createElement(`figure`);
-    const picture = document.createElement('img');
-    const divInfo = document.createElement('div');
-    const title = document.createElement('figcaption');
-    const genre = document.createElement('figcaption');
-    const linkDetail = document.createElement('a');
-    const btnCard = document.createElement('button');
     
-    title.className = 'movieTitle';
-    title.innerText = data.original_title;
-    genre.className = 'movieGenre';
-    genre.innerText = el.genre;
-    btnCard.className = 'btn-detail';
-    linkDetail.href = el.link;
-    picture.src = el.moviePict;
-    btnCard.innerText = 'Details';
-    btnCard.type = 'submit';
-    figureViewAll.appendChild(picture);
-    divInfo.appendChild(title);
-    divInfo.appendChild(genre);
-    figureViewAll.appendChild(divInfo);
-    list.appendChild(figureViewAll);
-    divInfo.appendChild(linkDetail);
-    linkDetail.appendChild(btnCard);
-
-});
+    
+    
+    
+    
+    proses()
