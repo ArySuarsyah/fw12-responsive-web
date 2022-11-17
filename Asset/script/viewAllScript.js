7b81353369adbef9fd159818851d5e1bd27e397d
arsrsyh <gotrany97@gmail.com>
2022-11-18 00:21:11 +0800

    Mengambil data dari API

--- Asset/script/viewAllScript.js
+++ Asset/script/viewAllScript.js
@@ -4,21 +4,26 @@ list.className = 'listMovie'
 
 
 const getMovieData = fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=0425e48e1b84cac8b5d27b8c9980a9ba&language=en-US&page=1');
+const genreData = fetch('https://api.themoviedb.org/3/genre/list?api_key=0425e48e1b84cac8b5d27b8c9980a9ba')
 
 const proses = async () => {
     const respons = await getMovieData;
-    const data = await respons.json();
-    list.innerHTML = data.poster
+    const data = await respons.json(); 
+    //buatkan di sini resu mas.? bukan mas 
+    const responsGenre = await genreData; //gitu yoi lanjut 
+    const dataGenre = await responsGenre.json();
+    console.log(dataGenre.genres);
     
     let movie = data.results
-    console.log(movie);
     movie.forEach((el) => {
+        
+
         const figureViewAll = document.createElement(`figure`);
         list.appendChild(figureViewAll);
         
         const picture = document.createElement('img');
         figureViewAll.appendChild(picture);
-        // picture.src = el.poster;
+        picture.src =`https://www.themoviedb.org/t/p/w220_and_h330_face${el.poster_path}` // poster
         
         
         const divInfo = document.createElement('div');
@@ -27,18 +32,26 @@ const proses = async () => {
         
         const title = document.createElement('figcaption');
         title.className = 'movieTitle';
-        title.innerText = movie.title
+        title.innerText = el.title; //title
         
         divInfo.appendChild(title);
         const genre = document.createElement('figcaption');
         genre.className = 'movieGenre';
-        genre.innerText = el.genre;
+        // genre.innerText = res.genres; //id Genre
+        
+                for (let i = 0; i < el.genre_ids.length; i++) {
+                    for (let j = 0; j < dataGenre.genres.length; j++) {
+                        if (el.genre_ids[i] == dataGenre.genres[j].id) {
+                            genre.innerText += ` ${dataGenre.genres[j].name} `
+                        }
+                    }
+                } 
         divInfo.appendChild(genre);
         
         
         const linkDetail = document.createElement('a');
         divInfo.appendChild(linkDetail);
-        linkDetail.href = el.link;
+        linkDetail.href = 'movieDetails.html';
 
 
         const btnCard = document.createElement('button');
